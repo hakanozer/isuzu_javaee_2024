5d5806e31c4f656cbec0376538978826ae817a06
Hakan ÖZER <hakanozer02@gmail.com>
2024-06-24 20:29:53 +0300

    Profile

---
+++ mvc/src/main/java/com/works/controllers/ProfileController.java
@@ -0,0 +1,17 @@
+
+package com.works.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ProfileController {
+    
+    @GetMapping("profile")
+    public String profile() {
+        return "profile";
+    }
+    
+    
+}
